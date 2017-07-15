56f984eb0b0858b3067cf141a306a6fc1dfd8a9d
Gwendolyn Holley (Wendy) <gmfholley@gmail.com>
2017-07-15 13:20:57 +0000

    Adds status after serve_your_data

---
+++ routes.rb
@@ -0,0 +1,18 @@
+get '/hello' do
+  'Hello world!'
+end
+
+get '/users' do
+  @users = User.all
+  names = []
+  @users.each do |user|
+    names << user.salutation
+  end
+  "Here are all the users: #{names.join(' ')}"
+end
+
+get '/users/:id' do
+  id = params[:id].to_i
+  @user = User.find(id)
+  "Hello, #{@user.salutation}"
+end
