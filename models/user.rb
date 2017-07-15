bac11febf9b4df08e4fc7c6edac414170e6f7afd
Gwendolyn Holley (Wendy) <gmfholley@gmail.com>
2017-07-15 13:13:53 +0000

    Adds status of add validations

--- models/user.rb
+++ models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-    has_many :tweets
+  has_many :tweets
+    
+  validates :name, presence: true
+  validates :salutation, presence: true
+  validates :email, presence: true
+  validates_uniqueness_of :email
 end
